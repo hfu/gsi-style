9265b69614062dd01092a874c8f8dafe01a3f756
Hidenori Fujimura <hfu@users.noreply.github.com>
2020-03-11 21:07:31 +0900

    update

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ style.sources.v = style.sources['gsibv-vectortile-source-1-4-17']
 delete style.sources['gsibv-vectortile-source-1-4-17']
 style.sources.v.tiles[0] = 
   'https://maps.gsi.go.jp/xyz/experimental_bvmap/{z}/{x}/{y}.pbf'
-delete style.layers
 
+layers = []
+
+for(layer of style.layers) {
+  if (layer.type === 'background') {
+  } else {
+    layer.source = 'v'
+    layer.id = layer.metadata.path
+    delete layer.metadata
+    layers.push(layer)
+  }
+}
+
+style.layers = layers
 console.log(JSON.stringify(style, null, 2))
