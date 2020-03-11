4016ed6b31f9df1a3a1ceae574a180a8474993b5
Hidenori Fujimura <hfu@users.noreply.github.com>
2020-03-11 21:51:25 +0900

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 let style = JSON.parse(fs.readFileSync('src/style.json'))
-style.sprite = 'https://maps.gsi.go.jp/vector/sprite/std'
+style.sprite = 'https://hfu.github.io/gsi-style/sprite/std'
 style.sources.v = style.sources['gsibv-vectortile-source-1-4-17']
 delete style.sources['gsibv-vectortile-source-1-4-17']
 style.sources.v.tiles[0] = 
